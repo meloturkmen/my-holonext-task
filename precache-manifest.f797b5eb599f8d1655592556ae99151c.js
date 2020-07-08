5ceff6785cfeb266ad54c2b13a960efabc868dac
meloturkmen <35032364+meloturkmen@users.noreply.github.com>
2020-07-08 23:34:16 +0300

    Updates

---
+++ precache-manifest.f797b5eb599f8d1655592556ae99151c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "519760bd67e09d4131d2b957fe106582",
+    "url": "/my-holonext-task/index.html"
+  },
+  {
+    "revision": "c1eb4c7b4fc19a68e7e3",
+    "url": "/my-holonext-task/static/css/main.79ab1726.chunk.css"
+  },
+  {
+    "revision": "793bb0aa3b6de7f0f260",
+    "url": "/my-holonext-task/static/js/2.c38e2a30.chunk.js"
+  },
+  {
+    "revision": "501f379103cbfcb5ff11211dd39a28a2",
+    "url": "/my-holonext-task/static/js/2.c38e2a30.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c1eb4c7b4fc19a68e7e3",
+    "url": "/my-holonext-task/static/js/main.9f9ffba6.chunk.js"
+  },
+  {
+    "revision": "44e7fc32b1115e4603da",
+    "url": "/my-holonext-task/static/js/runtime-main.f187ac22.js"
+  }
+]);
